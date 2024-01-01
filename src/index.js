b9b3a9fcbbcdb7869de76100fb4e7f52778446af
Abrar <hossainabrar@aol.com>
2024-01-01 04:04:25 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form")
+  inputForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const input = document.querySelector("input#searchByID");
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(resp => resp.json())
+    .then(element => {postMovie(element)})
+  })
+
+  function postMovie(movie){
+    const movieDetails = document.querySelector('section#movieDetails')
+    const title = document.createElement('h5')
+    title.innerText = movie.title
+    movieDetails.querySelector('h4').appendChild(title)
+    const summary = document.createElement('p')
+    summary.innerText = movie.summary
+    movieDetails.querySelector('p').appendChild(summary)
+  }
 }
 
 document.addEventListener('DOMContentLoaded', init);
